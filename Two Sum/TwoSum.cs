a560e73f92816ee05b2efdba0976fa1b6db5221c
AlhasanNO <aalhassann@gmail.com>
2025-01-31 15:12:10 +0200

    Two Sum

---
+++ Two Sum/TwoSum.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int x;
+        Dictionary<int, int> keyValuePairs = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            x = target - nums[i];
+
+            if (keyValuePairs.ContainsKey(x))
+            {
+                return [keyValuePairs[x], i];
+            }
+
+            keyValuePairs[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
