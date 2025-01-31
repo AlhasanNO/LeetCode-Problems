ed374ca649fa8d525b89b4ab8919eb790dd806ff
AlhasanNO <aalhassann@gmail.com>
2025-01-31 15:13:37 +0200

    Majority Element

---
+++ Majority Element/MajorityElement.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int element = nums[0];
+        int counter = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == element)
+                counter++;
+
+            else
+                counter--;
+
+            if (counter == 0)
+            {
+                element = nums[i];
+                counter = 1;
+            }
+        }
+
+        return element;
+    }
+}
