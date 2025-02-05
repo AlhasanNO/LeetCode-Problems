c0256e7c3253a2cca273be098524ccd502c1f938
AlhasanNO <aalhassann@gmail.com>
2025-02-05 21:35:24 +0200

    Length Of Last Word

---
+++ Easy/Length Of Last Word/LengthOfLastWord.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        int length = 0;
+        int n = s.Length - 1;
+
+        while (n >= 0 && s[n] == ' ')
+            n--;
+
+        while (n >= 0 && s[n] != ' ')
+        {
+            length++;
+            n--;
+        }
+
+        return length;
+    }
+}
