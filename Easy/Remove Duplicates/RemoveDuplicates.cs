1960620b6a4f6d694ca2188c2c181fbcf81b7a05
AlhasanNO <aalhassann@gmail.com>
2025-02-05 22:17:37 +0200

    Remove Duplicates

---
+++ Easy/Remove Duplicates/RemoveDuplicates.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int n = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+
+        return n;
+    }
+}
