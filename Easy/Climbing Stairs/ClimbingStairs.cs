e02b0ea3ed6217659756c3e64d95bdf84b43acec
AlhasanNO <aalhassann@gmail.com>
2025-02-05 20:42:35 +0200

    Climbing Stairs

---
+++ Easy/Climbing Stairs/ClimbingStairs.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n <= 3)
+            return n;
+
+        int case1 = 2;
+        int case2 = 3;
+        int caseN = 0;
+
+        for (int i = 3; i < n; i++)
+        {
+            caseN = case1 + case2;
+            case1 = case2;
+            case2 = caseN;
+        }
+
+        return caseN;
+    }
+}
