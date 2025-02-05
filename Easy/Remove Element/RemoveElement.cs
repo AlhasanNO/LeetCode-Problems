807feae2e00713894a7218d5faefe73c947fc1e4
AlhasanNO <aalhassann@gmail.com>
2025-02-05 21:54:47 +0200

    Remove Element

---
+++ Easy/Remove Element/RemoveElement.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int n = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+
+        return n;
+    }
+}
