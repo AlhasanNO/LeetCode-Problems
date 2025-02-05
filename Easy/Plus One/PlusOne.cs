95f91c602c8f5a2f12ab74935858de9eed4eac7b
AlhasanNO <aalhassann@gmail.com>
2025-02-05 17:20:29 +0200

    Add One

---
+++ Easy/Plus One/PlusOne.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        int i = digits.Length - 1;
+
+        while (i >= 0)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+            i--;
+        }
+
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
