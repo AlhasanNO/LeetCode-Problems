366f522387119b7612a93511e61d633e41476581
AlhasanNO <aalhassann@gmail.com>
2025-02-06 20:25:59 +0200

    Isomorphic Strings

---
+++ Easy/Isomorphic Strings/IsomorphicStrings.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        Dictionary<char, char> sDic = new();
+        Dictionary<char, char> tDic = new();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (sDic.ContainsKey(s[i]))
+            {
+                if (sDic[s[i]] != t[i])
+                    return false;
+            }
+
+            else
+                sDic[s[i]] = t[i];
+
+            if (tDic.ContainsKey(t[i]))
+            {
+                if (tDic[t[i]] != s[i])
+                    return false;
+            }
+
+            else
+                tDic[t[i]] = s[i];
+        }
+
+        return true;
+    }
+}
