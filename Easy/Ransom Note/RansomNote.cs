82062b822f8d553981eac38b36f3cf59a3c53275
AlhasanNO <aalhassann@gmail.com>
2025-02-06 19:38:02 +0200

    Ransom Note

---
+++ Easy/Ransom Note/RansomNote.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        Dictionary<char, int> myDictionary = new();
+
+        for (int i = 0; i < magazine.Length; i++)
+        {
+            if (myDictionary.ContainsKey(magazine[i]))
+            {
+                myDictionary[magazine[i]]++;
+            }
+
+            else
+            {
+                myDictionary[magazine[i]] = 1;
+            }
+        }
+
+        for (int i = 0; i < ransomNote.Length; i++)
+        {
+            if (!myDictionary.ContainsKey(ransomNote[i]) || myDictionary[ransomNote[i]] <= 0)
+                return false;
+
+            myDictionary[ransomNote[i]]--;
+        }
+
+        return true;
+    }
+}
