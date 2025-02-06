b117facf754b3eb8a7bdf41120dc5cef11aaced8
AlhasanNO <aalhassann@gmail.com>
2025-02-06 13:19:49 +0200

    Longest Common Prefix

---
+++ Easy/Longest Common Prefix/LongestCommonPrefix.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0)
+            return "";
+
+        string pref = strs[0];
+        int prefLength = pref.Length;
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (prefLength > strs[i].Length || !strs[i].StartsWith(pref))
+            {
+                prefLength--;
+
+                if (prefLength == 0)
+                    return "";
+
+                pref = pref.Substring(0, prefLength);
+            }
+        }
+
+        return pref;
+    }
+}
