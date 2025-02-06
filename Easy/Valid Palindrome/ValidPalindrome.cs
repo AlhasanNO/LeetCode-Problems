d3bd0e6375a0f5469691a2026ee2f92b02158112
AlhasanNO <aalhassann@gmail.com>
2025-02-06 18:37:51 +0200

    Valid Palindrome

---
+++ Easy/Valid Palindrome/ValidPalindrome.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        if (s == " ")
+            return true;
+
+        int first = 0;
+        int last = s.Length - 1;
+
+        while (last >= first)
+        {
+            while (first < last && !char.IsLetterOrDigit(s[first]))
+                first++;
+
+            while (first < last && !char.IsLetterOrDigit(s[last]))
+                last--;
+
+            if (char.ToLower(s[first]) != char.ToLower(s[last]))
+                return false;
+
+            first++;
+            last--;
+        }
+
+        return true;
+    }
+}
