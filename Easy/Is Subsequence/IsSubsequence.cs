b370572362c7521bf34be9d57cc4cad7e362bab6
AlhasanNO <aalhassann@gmail.com>
2025-02-06 19:02:17 +0200

    Is Subsequence

---
+++ Easy/Is Subsequence/IsSubsequence.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s))
+            return true;
+
+        int first = 0;
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (s[first] == t[i])
+                first++;
+
+            if (first == s.Length)
+                return true;
+        }
+
+        return false;
+    }
+}
